80833efa5b85f3f25b8cbcc25ef4bc180eb765de
Dimuth Ruwantha <DimuthRuwantha@users.noreply.github.com>
2019-09-04 12:10:15 +0530

    Progress upto dynamic classes (#1)
    * add an image
    
    * working with styles
    
    * dynamic classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,30 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1
+    count: 0
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
   };
   render() {
+    let classes = this.getBadgeClasses();
+
     return (
-      <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button class="btn btn-default">Increment</button>
-      </React.Fragment>
+      <div>
+        <span style={this.styles} className={classes}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
