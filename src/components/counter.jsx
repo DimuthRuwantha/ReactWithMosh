462786cb5cb877b2db04ba797d2ae7a43609e62b
Dimuth Ruwantha <dimuthruwantha@gmail.com>
2019-09-04 12:06:42 +0530

    dynamic classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,14 +10,22 @@ class Counter extends Component {
     fontWeight: "bold"
   };
   render() {
+    let classes = this.getBadgeClasses();
+
     return (
       <div>
-        <span style={this.styles} className="badge badge-primary m-2">
+        <span style={this.styles} className={classes}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
